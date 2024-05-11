f5523b45ed17746dd0a9ff613cb9f5ca5d083187
mh <mouradhilalipr.o@gmail.com>
2024-05-11 12:45:00 +0100

    hi

---
+++ app/auth.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StyleSheet, View , Text} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const Auth = () => {
+    return (
+        <SafeAreaView>
+            <Text>hi</Text>
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({})
+
+export default Auth;
